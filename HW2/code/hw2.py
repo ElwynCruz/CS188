d0daa6c633bc5cfc9d8e56fe4fca1df1f01a4f77
ElwynCruz <ElwynCruz@users.noreply.github.com>
2019-12-21 00:26:40 -0800

    add hw2

--- HW2/code/hw2.py
+++ HW2/code/hw2.py
@@ -12,18 +12,24 @@ def extractFrames(video_name):
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
 
     i=0
-    frames = []
+    gray_frames = []
+    color_frames = []
     while(cap.isOpened()):
         ret, frame = cap.read()
         if ret==True:
+            color_path = '../frames/color'+ str(i) + '.jpg'
+            gray_path = '../frames/gray'+ str(i) + '.jpg'
             
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+            
             #cv2.imshow('frame',gray)
-            img_path = '../frames/frame'+ str(i) + '.jpg'
-            cv2.imwrite(img_path, gray)
-            img = cv2.imread(img_path, 0)
-            frames.append(img)
+            cv2.imwrite(color_path, frame)
+            color_img = cv2.imread(color_path)
+            color_frames.append(color_img)
+
+            cv2.imwrite(gray_path, gray)
+            gray_img = cv2.imread(gray_path, 0)
+            gray_frames.append(gray_img)
             i+=1
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -33,7 +39,7 @@ def extractFrames(video_name):
     # Release everything if job is finished
     cap.release()
     cv2.destroyAllWindows()
-    return frames
+    return gray_frames, color_frames
 
 def findTemplate(images):
     template = cv2.imread('../img/template.jpg', 0)
@@ -64,32 +70,64 @@ def findTemplate(images):
 def plotMaximas(matches):
     x = []
     y = []
+    shift = []
     for i in matches:
         loc = np.argmax(i)
-        x.append(math.floor(loc / len(i[0])))
-        y.append(loc % len(i[0]))
-    plt.plot(y,x)
-    plt.show()
+        x2 = math.floor(loc / len(i[0]))
+        y2 = loc % len(i[0])
+        xy = []
+        xy.append(x2)
+        xy.append(y2)
+        shift.append(xy)
+    #plt.plot(y,x)
+    # plt.show()
+    shifts = np.array(shift)
+    shifts = shifts - shifts[0]
+    return shifts
 
-def defocusImage(matches, img):
-	x = []
-	y = []
-	transformation_matrix = []
-	for i in matches:
-		loc = np.argmax(i)
-		# x = rows
-		x.append(math.floor(loc / len(i[0])))
-		#y = columns
-		y.append(loc % len(i[0]))
-	for i in range(x):
-		shift = (x,y)
-		transformation_matrix.append(shift)
+# def defocusImage(matches, colorframes):
+
+def defocusedImage(shifts, colorframes):
+    firstFrame = colorframes[0]
+    finalImage = np.int32(colorframes[0])
+    
+    for num,i in enumerate(shifts):
+        transMat = np.float32([[1,0,-i[1]],[0,1,-i[0]]])
+        rows, cols, intensity = np.shape(firstFrame)
+        transImg = cv2.warpAffine(colorframes[num], transMat, (cols,rows))
+        finalImage += np.array(np.int32(transImg))
+    finalImage = finalImage / len(shifts)
+    finalImage = np.uint8(finalImage)
+    cv2.imwrite('../test.jpg', finalImage)
 	
-
+def plotFunc():
+    # w = []
+    # d = []
+    # for i in range(100):
+    #     if i == 0:
+    #         continue
+    #     for j in range(100):
+    #         if j == 0:
+    #             continue
+    #         diff = abs(i-j)
+    #         width = float(diff)/(i*j)
+    #         d.append(diff)
+    #         w.append(width)
+    # plt.scatter(d,w)
+    # plt.show()     
+    w = []
+    f = []
+    for i in range(100):
+        w.append(i)
+        f.append(i*.5)
+    plt.scatter(f,w)
+    plt.show()      
 
 if __name__ == '__main__':
     videoStr = "../img/video.MOV"
-    frames = extractFrames(videoStr)
-    matchedframes = findTemplate(frames)
-    defocusImage(matchedframes, frames[0])
-    #plotMaximas(matchedframes)
+    grayframes, colorframes = extractFrames(videoStr)
+    matchedframes = np.array(findTemplate(grayframes))
+    shifts = plotMaximas(matchedframes)
+    # plotMaximas(matchedframes)
+    defocusedImage(shifts,colorframes)
+    plotFunc()
